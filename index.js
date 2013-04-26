ee9df240d058af1c3ed0f2c996a3785cda33d87f
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-26 17:11:31 +0200

    killer works

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-var http = require('http');
+var http = require('http')
+  , exec = require('child_process').exec
+  , server;
+
+
+server = http.createServer(function (req, res) {
+  console.log("Received a request");
+  exec('pkill -9 ioquake3', function (err, stdout, stderr) {
+    console.log('-- DONE --');
+    console.log(stdout);
+    console.log("===");
+    console.log(stderr);
+  });
+  res.writeHead(200);
+  res.end("TEST", 'utf8');
+});
+
+console.log("Started server on port 6767");
+server.listen(6767);
